2b509d271f5443c161de3f022a9133063721fa9e
isoppp <prog@d2deck.com>
2016-10-07 11:05:54 +0900

    - add config.fish - update setting.jar

---
+++ config.fish
@@ -0,0 +1,10 @@
+########################################
+# config.fish
+########################################
+# 環境変数
+set -x NODE_PATH $HOME/.nodebrew/current/bin/node
+set -x GOPATH $HOME/go
+
+set -x LANG ja_JP.UTF-8
+set -x PATH /usr/bin/git $PATH
+set -x PATH $HOME/.nodebrew/current/bin $PATH
