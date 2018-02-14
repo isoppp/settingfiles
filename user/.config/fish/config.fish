81c9c2de4e4c05a1478f5b7677eb0a367c509092
isoppp <prog@d2deck.com>
2018-02-14 22:52:52 +0900

    update files

--- user/.config/fish/config.fish
+++ user/.config/fish/config.fish
@@ -6,5 +6,6 @@ set -x NODE_PATH $HOME/.nodebrew/current/bin/node
 set -x GOPATH $HOME/go
 
 set -x LANG ja_JP.UTF-8
-set -x PATH /usr/bin/git $PATH
 set -x PATH $HOME/.nodebrew/current/bin $PATH
+
+rbenv init - | source
