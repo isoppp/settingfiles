a0df7b9ee823367a20685d58bfa06f5bfc6911c4
isoppp <prog@d2deck.com>
2018-10-29 01:26:06 +0900

    add init/fish

---
+++ init/_fisher.sh
@@ -0,0 +1,20 @@
+#!/usr/local/bin/fish
+
+if type -q fisher
+  echo "skip: fisher already installed"
+else
+# fisher
+  curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+
+  # fisher first
+  if not functions -q fisher
+      set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+      curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+      fish -c fisher
+  end
+
+  fisher add decors/fish-ghq
+  fisher add jethrokuan/z
+  fisher add ponko2/fish-plugin-peco
+end
+
---
+++ init/fish.sh
@@ -0,0 +1,22 @@
+# fish install
+
+script_dir=$(cd $(dirname $0); pwd)
+
+if type fish >/dev/null 2>&1; then
+  echo "skip: fish already installed"
+else
+  brew install fish
+
+  echo "You will need to add: /usr/local/bin/fish to /etc/shells."
+  echo "Then run: chsh -s /usr/local/bin/fish to make fish your default shell."
+
+  read -n 1 -s -r -p "Press any key to continue"
+
+  if [ -s "~/.config/fish/config.fish" ]; then
+    cd $script_dir
+    fishfiles_path=$(realpath ../fishfiles)/config.fish
+    # echo source $fishfiles_path >> ~/.config/fish/config.fish
+  fi
+fi
+
+fish $script_dir/_fisher.sh
