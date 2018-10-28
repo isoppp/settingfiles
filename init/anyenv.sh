a5ffdf786d7ec69569c49bade20eb87e4b9a95cb
isoppp <prog@d2deck.com>
2018-10-29 01:26:18 +0900

    add init anyenv

---
+++ init/anyenv.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/riywo/anyenv ~/.anyenv
+echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(anyenv init -)"' >> ~/.bash_profile
+exec $SHELL -l
+
+anyenv install rbenv
+anyenv install ndenv
