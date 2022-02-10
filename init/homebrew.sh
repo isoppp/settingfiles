adf131c6b75b83de626f3e03f4a0875232a4fe2e
isoppp <prog@d2deck.com>
2022-02-11 00:46:26 +0900

    adjust default brew packages

--- init/homebrew.sh
+++ init/homebrew.sh
@@ -1,22 +1,6 @@
-# from https://github.com/jfmercer/fishfiles
-#
-# Homebrew
-#
-# This installs some of the common dependencies needed (or at least desired)
-# using Homebrew.
-
-# Check for Homebrew
-if test ! $(which brew)
-then
-  echo "Installing Homebrew for you."
-fi
-
-echo "Updating existing Homebrew packages"
-brew update
-
-myBrew=( coreutils fzf ghq peco pngquant hub)
-
-myCaskBrew=( qlcolorcode qlmarkdown quicklook-json qlimagesize webpquicklook suspicious-package quicklookase qlvideo )
+#myBrew=( coreutils fzf ghq peco pngquant hub)
+myBrew=( fzf gh )
+#myCaskBrew=( qlcolorcode qlmarkdown quicklook-json qlimagesize webpquicklook suspicious-package quicklookase qlvideo )
 
 # Run installs
 echo "Installing Homebrew packages"
@@ -30,11 +14,3 @@ for i in ${myCaskBrew[*]}
 do
     brew cask install $i 2>/dev/null
 done
-
-# Clean up the Cellar
-echo "Cleaning up . . . "
-brew cleanup
-
-echo "Homebrew install complete!"
-
-exit 0
