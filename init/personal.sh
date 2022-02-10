276e777bae717e28ca77741161a0b999a6ae2b32
isoppp <prog@d2deck.com>
2022-02-10 23:50:35 +0900

    fix global gitignore path

--- init/personal.sh
+++ init/personal.sh
@@ -2,10 +2,6 @@ mkdir -p ~/workspace/works
 mkdir -p ~/workspace/personal
 mkdir -p ~/workspace/oss
 
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-cd $SCRIPT_DIR
-cp ../common_dotfiles/.gitignore_global ~/.gitignore_global
-
 git config --global user.name "isoppp"
 git config --global user.email "prog@d2deck.com"
-git config --global core.excludesfile ~/.gitignore_global
+git config --global core.excludesfile ~/.config/git/ignore
