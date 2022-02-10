99a340807567780406999165081c231d42b8a3b9
isoppp <prog@d2deck.com>
2022-02-11 00:45:53 +0900

    update git alias

--- fishfiles/configs/myconfig/git-aliases.fish
+++ fishfiles/configs/myconfig/git-aliases.fish
@@ -1,10 +1,3 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-set -x hub_path (which hub)
-if test -f $hub_path
-  alias git $hub_path
-end
-
 # NOTE: Some of these aliases depend on `hub` being installed.
 
 alias egc "vim $HOME/.gitconfig"
