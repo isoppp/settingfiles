95ca38f1be5cc2df6ce6c23647a9348845cea5ee
isoppp <prog@d2deck.com>
2022-02-10 23:49:57 +0900

    fix fish setup

---
+++ fishfiles/configs/myconfig/go-aliases.fish
@@ -0,0 +1,2 @@
+# https://docs.python.org/ja/3/library/venv.html
+alias gotestcov "go test -coverprofile=coverage.out && go tool cover -html=coverage.out"
--- fishfiles/myconfig/python-aliases.fish
+++
@@ -1,4 +0,0 @@
-# https://docs.python.org/ja/3/library/venv.html
-alias pei "pipenv install"
-alias pes "pipenv shell"
-alias per "pipenv run"
